c79e31a3b5c635a998734a21a74bb6c9a4943e33
Laura Pajaro <lauratecno30@google.com>
2025-04-03 15:28:54 -0300

    Consolelog Subs String

--- Backend/lib/tables/index.js
+++ Backend/lib/tables/index.js
@@ -320,7 +320,7 @@ const init = async () => {
             });
 
             //const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: { rejectUnauthorized: false, ca: fs.readFileSync("./ca-certificate.crt") } });
-            const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: { rejectUnauthorized: true, } });
+            const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: { rejectUnauthorized: false, } });
             subscriber.events.on('error', onError);
             subscriber.events.on('connect', onConnect);
             subscriber.events.on('reconnect', onReconnect);

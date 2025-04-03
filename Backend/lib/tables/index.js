a8d86d861db493248e9e0c42192c89fe6d524bdb
Laura Pajaro <lauratecno30@google.com>
2025-04-03 15:49:29 -0300

    certif

--- Backend/lib/tables/index.js
+++ Backend/lib/tables/index.js
@@ -11,6 +11,7 @@ import { info, warn, error } from '../log/index.js';
 export const { prefix: cdcPrefix, id: cdcId, action: cdcAction, editedAt: cdcEditedAt, editedBy: cdcEditedBy, lastChangesLimit: cdcLastChangesLimit } = env.tables.cdc;
 import path from 'path'
 const notificationListeners = [];
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const createHash = (str) => crypto.createHash('sha256').update(str).digest('hex');
 
@@ -320,9 +321,9 @@ const init = async () => {
 
                 notificationListeners.forEach(cb => { try { cb(payload) } catch (e) { error(e); } });
             });
-            console.log({certif: fs.readFileSync(path.join(__dirname, './certs/ca-certificate.crt')).toString()})
+            console.log({certif: fs.readFileSync(path.join(__dirname, './ca-certificate.crt')).toString()})
             //const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: { rejectUnauthorized: false, ca: fs.readFileSync("./ca-certificate.crt") } });
-            const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: {    ca: fs.readFileSync(path.join(__dirname, './certs/ca-certificate.crt')).toString(),
+            const subscriber = env.app.env === 'development' ? createSubscriber({ connectionString }) : createSubscriber({ connectionString, ssl: {    ca: fs.readFileSync(path.join(__dirname, './ca-certificate.crt')).toString(),
                 rejectUnauthorized: true,}}) // poner en false solo si estás probando } });
             subscriber.events.on('error', onError);
             subscriber.events.on('connect', onConnect);
